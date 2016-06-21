128a2449f4c736f95a34dd419d13564187d8807f
sasaki-seiji <sasaki-seiji@msj.biglobe.ne.jp>
2016-06-21 20:15:38 +0900

    item06に例題の説明を追加

--- ch01/item06/item_06.py
+++ ch01/item06/item_06.py
@@ -21,7 +21,7 @@ from sys import stdout as STDOUT
 
 
 # Example 1
-print('Example 1: ')
+print('Example 1: stride')
 a = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
 odds = a[::2]
 evens = a[1::2]
@@ -30,14 +30,14 @@ print(evens)
 
 
 # Example 2
-print('\nExample 2:')
+print('\nExample 2: reverse by stride=-1')
 x = b'mongoose'
 y = x[::-1]
 print(y)
 
 
 # Example 3
-print("\nExample 3:")
+print("\nExample 3: reverse utf-8 by stride=-1")
 try:
     w = '謝謝'
     x = w.encode('utf-8')
@@ -50,23 +50,23 @@ else:
 
 
 # Example 4
-print("\nExample 4:")
+print("\nExample 4: stride = 2, -2")
 a = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
 print(a)
-print(a[::2])   # ['a', 'c', 'e', 'g']
-print(a[::-2])  # ['h', 'f', 'd', 'b']
+print("a[::2]", a[::2])   # ['a', 'c', 'e', 'g']
+print("a[::-2]", a[::-2])  # ['h', 'f', 'd', 'b']
 
 
 # Example 5
-print("\nExample 5:")
-print(a[2::2])     # ['c', 'e', 'g']
-print(a[-2::-2])   # ['g', 'e', 'c', 'a']
-print(a[-2:2:-2])  # ['g', 'e']
-print(a[2:2:-2])   # []
+print("\nExample 5: start, end, stride")
+print("a[2::2]", a[2::2])     # ['c', 'e', 'g']
+print("a[-2::-2]", a[-2::-2])   # ['g', 'e', 'c', 'a']
+print("a[-2:2:-2]", a[-2:2:-2])  # ['g', 'e']
+print("a[2:2:-2]", a[2:2:-2])   # []
 
 
 # Example 6
-print("\nExample 6:")
+print("\nExample 6: separate stride and slice")
 b = a[::2]   # ['a', 'c', 'e', 'g']
 c = b[1:-1]  # ['c', 'e']
 print(a)
